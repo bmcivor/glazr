303b127d8309324c8e72f34bb6b16f0eb03d287f
Blake McIvor <bmcivor56@gmail.com>
2026-08-13 15:10:29 +1000

    feat(10): add in test framework for orders
    This is the test driven development part.
    
    Should have all the major functionality covered
    here with a bit of a sketch on how exceptions will
    be handled and when.

---
+++ backend/orders/services.py
@@ -0,0 +1,30 @@
+class OrderError(Exception):
+    """Base for anything the order service rejects."""
+
+
+class DonutNotFound(OrderError):
+    pass
+
+
+class DonutUnavailable(OrderError):
+    pass
+
+
+class InvalidQuantity(OrderError):
+    pass
+
+
+class InvalidOrder(OrderError):
+    pass
+
+
+class InvalidStatusChange(OrderError):
+    pass
+
+
+def create_order():
+    pass
+
+
+def dispatch_order():
+    pass
