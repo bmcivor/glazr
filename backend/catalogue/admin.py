0c15c7aabb709c944079c140096981e4dba58f3e
Blake McIvor <bmcivor56@gmail.com>
2026-08-13 13:43:43 +1000

    feat(7): register donut and users in the django admin
    Simple enough, the idea here is we get all this out
    of the box by keeping to the cookie cutter modelling
    of Django.
    
    We can flip values on rows here to trigger messages
    in the end as well if we like.

--- backend/catalogue/admin.py
+++ backend/catalogue/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Donut
+
+
+@admin.register(Donut)
+class DonutAdmin(admin.ModelAdmin):
+    """Admin for Donuts"""
+
+    list_display = ["donut_code", "price", "available"]
+    list_editable = ["available"]
+    search_fields = ["donut_code"]
